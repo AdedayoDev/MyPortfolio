4fe6e0f64b6ce5fae6ac787c2f814ecbac0c9511
John Adedayo <johnadedayo1@gmail.com>
2025-12-30 04:19:48 -0800

    about section

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion';
 import { fadeUp } from '@/libs/animation';
 import type { ProjectType } from '@/types';
 
+
 export const ProjectCard = ({
   imgSrc,
   title,
@@ -13,26 +14,26 @@ export const ProjectCard = ({
       variants={fadeUp}
       className='relative'
     >
-      <figure className='overflow-hidden rounded-md'>
-        <img
-          src={imgSrc}
-          alt={title}
-          className='rounded-md transition duration-500 hover:scale-115 w-full '
-        />
-      </figure>
+      <a href={projectLink}>
+        <figure className='overflow-hidden rounded-md'>
+          <img
+            src={imgSrc}
+            alt={title}
+            className='rounded-md transition duration-500 hover:scale-115 w-full '
+          />
+        </figure>
 
-      <div className='absolute bottom-0 p-2 flex gap-2'>
-        {tags.map((tag, i) => (
-          <span
-            key={i}
-            className='bg-background hover:bg-primary hover:text-black py-1 px-2 rounded-sm text-sm cursor-pointer '
-          >
-            {tag}
-          </span>
-        ))}
-      </div>
-
-      <div className='absolute bottom-0 p-2 flex gap-2'>{projectLink}</div>
+        <div className='absolute bottom-0 p-2 flex gap-2'>
+          {tags.map((tag, i) => (
+            <span
+              key={i}
+              className='bg-background hover:bg-primary hover:text-black py-1 px-2 rounded-sm text-sm cursor-pointer '
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </a>
     </motion.div>
   );
 };
